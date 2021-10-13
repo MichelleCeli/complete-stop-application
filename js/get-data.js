62b43f5cc7da3c3b08948fda90201ce77205786a
Hallmann <michelle-celina.hallmann@stud.hshl.de>
2021-10-13 14:38:19 +0200

    Set Content of stop-information
    - get-data.js sets the information of the given stop
    - next to do: set data of orders

--- js/get-data.js
+++ js/get-data.js
@@ -2,10 +2,39 @@
   .then(response => response.json());
  */
 
- async function getStops(){
+
+var stop_number = 1;
+var stop_data;
+
+var stop_name;
+var street;
+var postal_code;
+var city;
+var country;
+var latitude;
+var longitude;
+
+ async function getStops(stop_number){
  const response = await fetch("/data/driver_route_sample.json");
  const data = await response.json();
-console.log(data.stops[1]);
+  console.log(data.stops[stop_number].name);
+
+  stop_name = data.stops[stop_number].name;
+  street = data.stops[stop_number].address.street;
+  postal_code = data.stops[stop_number].address.postal_code;
+  city = data.stops[stop_number].address.town;
+  country = data.stops[stop_number].address.country;
+  latitude = data.stops[stop_number].address.lat;
+  longitude = data.stops[stop_number].address.lon;
+
+  document.getElementById("stop-name").innerHTML = stop_name; 
+  document.getElementById("street-pc").innerHTML = street + " " + postal_code; 
+  document.getElementById("city-country").innerHTML = city + ", " + country; 
+  document.getElementById("lat_long").innerHTML = latitude + ", " + longitude;  
+
+
  }
 
- getStops();
+ 
+
+ getStops(stop_number);
